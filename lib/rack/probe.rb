fcfd727947f3b380ae1457bf0f83e19a4816a46c
ecin <ecin@copypastel.com>
2009-08-18 17:03:26 -0400

    Probes are now created when instancing Rack::Probe, not when loading the class.

--- lib/rack/probe.rb
+++ lib/rack/probe.rb
@@ -11,35 +11,37 @@ module Rack
     
     gem 'ruby-dtrace', '>= 0.2.7'
     require 'dtrace/provider'
-
-    Dtrace::Provider.create :rack do |p|
-      p.probe :get                # GET request
-      p.probe :post               # POST request
-      p.probe :put                # PUT request
-      p.probe :delete             # DELETE request
-      p.probe :ip,  :string       # IP of the requester
-      p.probe :path,  :string     # Path visited 
-      p.probe :referer, :string   # Referer
-      p.probe :xhr                # AJAX request
-    end
-    
-    # Provider shortcut
-    R = Dtrace::Probe::Rack
-    
+        
     def initialize( app, opts = {} )
+      # Unless our probes are already defined...
+      unless Dtrace::Probe::Rack.instance_of? Dtrace::Provider::Klass
+        Dtrace::Provider.create :rack do |p|
+          p.probe :get                # GET request
+          p.probe :post               # POST request
+          p.probe :put                # PUT request
+          p.probe :delete             # DELETE request
+          p.probe :ip,  :string       # IP of the requester
+          p.probe :path,  :string     # Path visited 
+          p.probe :referer, :string   # Referer
+          p.probe :xhr                # AJAX request
+        end
+        
+        # Provider shortcut
+        @R = Dtrace::Probe::Rack
+      end
       @app = app
     end
 
     def call( env )
       request = Rack::Request.new env
-      R.get(&:fire) if request.get?
-      R.post(&:fire) if request.post?
-      R.put(&:fire)  if request.put?
-      R.delete(&:fire) if request.delete?
-      R.xhr(&:fire) if request.xhr?
-      R.path    { |p| p.fire(request.path) }
-      R.ip      { |p| p.fire(request.ip) }
-      R.referer { |p| p.fire(request.referer) }
+      @R.get(&:fire) if request.get?
+      @R.post(&:fire) if request.post?
+      @R.put(&:fire)  if request.put?
+      @R.delete(&:fire) if request.delete?
+      @R.xhr(&:fire) if request.xhr?
+      @R.path    { |p| p.fire(request.path) }
+      @R.ip      { |p| p.fire(request.ip) }
+      @R.referer { |p| p.fire(request.referer) }
       @app.call env
     end
 
